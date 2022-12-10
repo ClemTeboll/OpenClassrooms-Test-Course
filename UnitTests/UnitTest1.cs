bcb2f699a79f662ce07dd3779957f9edf8b4f15f
Clément <clement.llobet@jasparo.fr>
2022-12-10 12:41:42 +0100

    add MultiplyAndDivide tests

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -31,4 +31,58 @@ namespace UnitTests
             Assert.IsType<decimal>(result);
         }
     }
+    public class CalculatorMemoryTests : IDisposable
+    {
+        void IDisposable.Dispose() { }
+        public void Dispose() { }
+
+        Calculator Calculator { get; }
+
+        public CalculatorMemoryTests()
+        {
+            Calculator = new Calculator();
+        }
+
+        [Fact]
+        public void AddNumberTest()
+        {
+            Calculator.Reset();
+
+            // Act
+            Calculator.Sum(3);
+            Calculator.Sum(7);
+
+            Assert.Equal(10, Calculator.Current);
+        }
+    }
+
+    public class MultiplyDivideTests
+    {
+        [Fact]
+        public void Test_MultiplyTwoElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Multiply(10, 5);
+
+            // Then, Assert
+            Assert.Equal(50, result);
+        }
+
+        [Fact]
+        public void Test_MultiplyManyElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Multiply(0.5M, 1, 2, 3, 4, -5.5M);
+
+            // Then, Assert
+            Assert.Equal(-66, result);
+            Assert.IsType<decimal>(result);
+        }
+    }
 }
