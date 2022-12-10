a9f459325cd87e02c0743b9a3009742a73897e42
Clément <clement.llobet@jasparo.fr>
2022-12-10 12:15:30 +0100

    add an in-memory support in Sum function

--- CalculatorProgram/Program.cs
+++ CalculatorProgram/Program.cs
@@ -7,11 +7,19 @@ namespace CalculatorProgram
     {
         static void Main()
         {
-            Console.WriteLine("L'application est lancée !");
+            Console.WriteLine("App is lauched!");
         }
 
         public decimal Sum(params decimal[] numbers)
         {
+            decimal Current = 0;
+
+            if (numbers.Length == 1)
+            {
+                Current += numbers[0];
+                return Current;
+            }
+
             var result = 0.0M;
             for (int i = 0; i < numbers.Length; i++)
             {
