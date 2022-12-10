80f2ee565b50339c8ebdb3c24d5e8e11f056410f
Clément <clement.llobet@jasparo.fr>
2022-12-10 10:41:31 +0100

    create Sum function

--- CalculatorProgram/Program.cs
+++ CalculatorProgram/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace CalculatorProgram
+{
+    public class Calculator
+    {
+        public decimal Sum(params decimal[] numbers)
+        {
+            var result = 0.0M;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+            return result;
+        }
+    }
+}
